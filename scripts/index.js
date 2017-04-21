c6f4ae977b94357022cf626392559ab827686146
Can ÜNSAL <unsalcan35@gmail.com>
2017-04-21 17:35:15 +0300

    combine gest.js with face util

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,32 @@
 $(document).ready(function() {
 	console.log("ready");
 
+	const FaceUtilAddon = require('../build/Release/FaceUtilAddon');
+
+	FaceUtilAddon.loadFaceDetector("data/lbpcascade_frontalface.xml");
+	//FaceUtilAddon.trainFaceModel("data/csv.csv");
+    //FaceUtilAddon.saveFaceModel("data/trained.data");
+	FaceUtilAddon.loadFaceModel("data/trained.data");
+
 	gest.start();
 	//gest.options.debug(true);
 	gest.options.subscribeWithCallback(function(gesture) {
-		
-		if(gesture.direction == "Right" || gesture.direction == "Left") {
-			console.log(gesture.direction);
+		console.log(gesture.direction);
+		if(gesture.direction == "Right") {
 			gest.stop();
+
+		    setTimeout(function() {
+		    	FaceUtilAddon.onDetected(80, function(msg) {
+			        if(msg == "can") {
+			        	console.log("welcome can");
+			        }
+			        FaceUtilAddon.stopListening();
+			        FaceUtilAddon.close();
+			    });
+			    FaceUtilAddon.startListening();
+		    }, 500);
+
 		}
 	});
+
 });

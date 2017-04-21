eb79f474b4e171d7f88a39c09a3cadd2b4cbf495
Can ÜNSAL <unsalcan35@gmail.com>
2017-04-21 17:33:42 +0300

    gest.js stop fix

--- scripts/index.js
+++ scripts/index.js
@@ -2,12 +2,12 @@ $(document).ready(function() {
 	console.log("ready");
 
 	gest.start();
-	gest.options.debug(true);
+	//gest.options.debug(true);
 	gest.options.subscribeWithCallback(function(gesture) {
 		
 		if(gesture.direction == "Right" || gesture.direction == "Left") {
 			console.log(gesture.direction);
-	
+			gest.stop();
 		}
 	});
 });

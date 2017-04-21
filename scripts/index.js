10c334de729527a8e488757f820bde7a31b473b3
Can ÜNSAL <unsalcan35@gmail.com>
2017-04-21 16:37:43 +0300

    add gesturejs

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
 	console.log("ready");
+
+	gest.start();
+	gest.options.debug(true);
+	gest.options.subscribeWithCallback(function(gesture) {
+		
+		if(gesture.direction == "Right" || gesture.direction == "Left") {
+			console.log(gesture.direction);
+	
+		}
+	});
 });

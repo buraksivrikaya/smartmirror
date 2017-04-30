dd6edbf7bde5fd1ed5a2fe54928e20a936b7a7b7
can <unsalcan35@gmail.com>
2017-04-30 18:57:45 +0300

    even more fixs

--- build/Release/.deps/Release/obj.target/FaceUtilAddon/addons/FaceUtilAddon/FaceUtil.o.d
+++
@@ -1,4 +0,0 @@
-cmd_Release/obj.target/FaceUtilAddon/addons/FaceUtilAddon/FaceUtil.o := g++ '-DNODE_GYP_MODULE_NAME=FaceUtilAddon' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/home/can/.node-gyp/7.7.3/include/node -I/home/can/.node-gyp/7.7.3/src -I/home/can/.node-gyp/7.7.3/deps/uv/include -I/home/can/.node-gyp/7.7.3/deps/v8/include  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++0x -MMD -MF ./Release/.deps/Release/obj.target/FaceUtilAddon/addons/FaceUtilAddon/FaceUtil.o.d.raw   -c -o Release/obj.target/FaceUtilAddon/addons/FaceUtilAddon/FaceUtil.o ../addons/FaceUtilAddon/FaceUtil.cpp
-Release/obj.target/FaceUtilAddon/addons/FaceUtilAddon/FaceUtil.o: \
- ../addons/FaceUtilAddon/FaceUtil.cpp
-../addons/FaceUtilAddon/FaceUtil.cpp:

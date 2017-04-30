bdd12c4b3cb0bd194a6c4ea253a98a258167fd20
can <unsalcan35@gmail.com>
2017-04-30 18:54:34 +0300

    fixs

---
+++ build/Release/.deps/Release/FaceUtilAddon.node.d
@@ -0,0 +1,1 @@
+cmd_Release/FaceUtilAddon.node := rm -rf "Release/FaceUtilAddon.node" && cp -af "Release/obj.target/FaceUtilAddon.node" "Release/FaceUtilAddon.node"
---
+++ build/Release/.deps/Release/obj.target/FaceUtilAddon/addons/FaceUtilAddon/FaceUtilAddon.o.d
@@ -0,0 +1,28 @@
+cmd_Release/obj.target/FaceUtilAddon/addons/FaceUtilAddon/FaceUtilAddon.o := g++ '-DNODE_GYP_MODULE_NAME=FaceUtilAddon' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/home/can/.node-gyp/7.7.3/include/node -I/home/can/.node-gyp/7.7.3/src -I/home/can/.node-gyp/7.7.3/deps/uv/include -I/home/can/.node-gyp/7.7.3/deps/v8/include  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++0x -MMD -MF ./Release/.deps/Release/obj.target/FaceUtilAddon/addons/FaceUtilAddon/FaceUtilAddon.o.d.raw   -c -o Release/obj.target/FaceUtilAddon/addons/FaceUtilAddon/FaceUtilAddon.o ../addons/FaceUtilAddon/FaceUtilAddon.cpp
+Release/obj.target/FaceUtilAddon/addons/FaceUtilAddon/FaceUtilAddon.o: \
+ ../addons/FaceUtilAddon/FaceUtilAddon.cpp \
+ /home/can/.node-gyp/7.7.3/include/node/node.h \
+ /home/can/.node-gyp/7.7.3/include/node/v8.h \
+ /home/can/.node-gyp/7.7.3/include/node/v8-version.h \
+ /home/can/.node-gyp/7.7.3/include/node/v8config.h \
+ /home/can/.node-gyp/7.7.3/include/node/node_version.h \
+ /home/can/.node-gyp/7.7.3/include/node/uv.h \
+ /home/can/.node-gyp/7.7.3/include/node/uv-errno.h \
+ /home/can/.node-gyp/7.7.3/include/node/uv-version.h \
+ /home/can/.node-gyp/7.7.3/include/node/uv-unix.h \
+ /home/can/.node-gyp/7.7.3/include/node/uv-threadpool.h \
+ /home/can/.node-gyp/7.7.3/include/node/uv-linux.h \
+ ../addons/FaceUtilAddon/FaceUtil.cpp
+../addons/FaceUtilAddon/FaceUtilAddon.cpp:
+/home/can/.node-gyp/7.7.3/include/node/node.h:
+/home/can/.node-gyp/7.7.3/include/node/v8.h:
+/home/can/.node-gyp/7.7.3/include/node/v8-version.h:
+/home/can/.node-gyp/7.7.3/include/node/v8config.h:
+/home/can/.node-gyp/7.7.3/include/node/node_version.h:
+/home/can/.node-gyp/7.7.3/include/node/uv.h:
+/home/can/.node-gyp/7.7.3/include/node/uv-errno.h:
+/home/can/.node-gyp/7.7.3/include/node/uv-version.h:
+/home/can/.node-gyp/7.7.3/include/node/uv-unix.h:
+/home/can/.node-gyp/7.7.3/include/node/uv-threadpool.h:
+/home/can/.node-gyp/7.7.3/include/node/uv-linux.h:
+../addons/FaceUtilAddon/FaceUtil.cpp:

1da08324778a70bd9c8d09c5bbad456a8379bcbc
can <unsalcan35@gmail.com>
2017-04-30 21:36:30 +0300

    remove old structure

--- frontend/html/js/notificationElements.js
+++
@@ -1,9 +0,0 @@
-var currentTime = new Date();
-var currentHours = currentTime.getHours();
-var currentMinutes = currentTime.getMinutes();
-
-var notificationElements = {
-	user: 'undefined user',
-	connection: 'Test Ağı',
-	clock: currentHours + ':' + currentMinutes,
-}
